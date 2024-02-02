527106ad6f2e7ecabd387f3f8a141a62ecca4bf4
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-02-02 11:05:37 -0300

    implementando busca

--- app/Livewire/Products.php
+++ app/Livewire/Products.php
@@ -8,11 +8,20 @@ use Livewire\Component;
 class Products extends Component
 {
     public array $products;
+    public string $search = '';
 
     public function mount()
     {
         $response = Http::get('https://dummyjson.com/products')->json();
         $this->products = $response['products'];
+    }
+
+    public function updatingSearch()
+    {
+        $response = Http::get('https://dummyjson.com/products')->json();
+        $this->products = collect($response['products'])->filter(function ($product) {
+            return str_contains(strtolower($product['title']), strtolower($this->search));
+        })->toArray();
     }
 
     public function render()
--- resources/views/livewire/products.blade.php
+++ resources/views/livewire/products.blade.php
@@ -22,6 +22,7 @@
                                         </svg>
                                     </div>
                                     <input type="text" id="simple-search"
+                                        wire:model.live="search"
                                         class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 pl-10 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
                                         placeholder="Search" required="">
                                 </div>
